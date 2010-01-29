1646a48c21dc0f5a873bd1b84fd9568067fa4bde
Rico Sta. Cruz <rico@ambiescent.com>
2010-01-29 17:09:02 +0800

    Updated CSS

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8" />
     <title></title>
     <style type="text/css">
+        html, body { height: 100%: margin: 0; padding: 0; }
         body { background: white url(sinefunc.png) center center no-repeat; }
+    </style>
 </head>
 <body>
 </body>
