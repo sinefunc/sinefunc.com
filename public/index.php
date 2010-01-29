704695830df7fb3ad779763c2d155a4785fb30d0
Rico Sta. Cruz <rico@ambiescent.com>
2010-01-29 17:07:22 +0800

    Updated with a silly splash page

--- public/index.php
+++ public/index.php
@@ -1,2 +1,11 @@
-Sinefunc.
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title></title>
+    <style type="text/css">
+        body { background: white url(sinefunc.png) center center no-repeat; }
+</head>
+<body>
+</body>
+</html>
