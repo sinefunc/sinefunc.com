fd7cb930d2bc7cb35ec1cbe90d948f3186c1ac34
Rico Sta. Cruz <rico@ambiescent.com>
2010-01-29 17:09:40 +0800

    Fixed CSS (heh)

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <title></title>
     <style type="text/css">
-        html, body { height: 100%: margin: 0; padding: 0; }
+        html, body { height: 100%; margin: 0; padding: 0; }
         body { background: white url(sinefunc.png) center center no-repeat; }
     </style>
 </head>
